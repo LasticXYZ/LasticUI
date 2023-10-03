824a8d3a4c9e3dc7487fc20d6f22f110f2895986
Aurora Poppyseed <aurora.makovac@gmail.com>
2023-10-04 00:09:39 +0200

    switching to next js 13

--- static_frontend/src/components/button/PrimaryButton.jsx
+++ static_frontend/src/components/button/PrimaryButton.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const PrimaryButton = ({ title="title", location="/" }) => (
     <Link
         href={ location } 
-        className="rounded-full bg-purple-7 hover:bg-purple-8 inline-flex items-center justify-center px-8 py-4 mr-3 font-medium text-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+        className="rounded-full bg-pink-4 hover:bg-pink-5 shadow-l inline-flex items-center justify-center px-8 py-4 mr-3 font-medium text-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
     >
         { title }
     </Link>
--- static_frontend/src/sections/Hero.jsx
+++ static_frontend/src/sections/Hero.jsx
@@ -2,60 +2,21 @@ import Link from 'next/link';
 import Image from 'next/image';
 import React from 'react';
 import { FaGithub, FaTelegram, FaTwitter, FaMedium } from 'react-icons/fa';
+import PrimaryButton from '@/components/button/PrimaryButton';
 
 const Hero = () => {
   return (
-    <div id='home' className='bg-[#34737B] w-full md:justify-start m-auto max-w-[1240px]  relative flex flex-col md:flex-row items-center justify-center'>
-      
+    <div id='home' className='bg-[#34737B]' >
+    <div className=' w-full md:justify-start m-auto relative flex flex-col md:flex-row items-center justify-center'>
       <div className='max-w-[500px] w-full px-20 text-center md:text-left md:px-0 py-20 h-full mx-auto p-2 relative z-10'>
         <div>
-          <p className='uppercase text-md md:px-0 font-semibold tracking-widest text-[#76EDDD]'>
-            POLKADOT&#39;S BLOCKSPACE MARKETPLACE
-          </p>
-          <h1 className='py-4 md:px-0 text-gray-800'>
-             <span className='text-[#FA857A] font-extrabold text-6xl font-syncopate'> LASTIC </span>
-          </h1>
-          <p className='py-4 md:px-0 text-[#FFD28A] md:text-sm mx-auto'>
-          Lastic - the first decentralized coretime market on Polkadot! Lastic transforms blockspace allocation using Polkadot&apos;s coretime model for buying and selling across the ecosystem.
-          </p>
-          <div className='flex items-center justify-center md:justify-start max-w-[400px] px-4 py-8 mx-auto'>
-            <Link
-              href='https://github.com/LasticXYZ/Lastic'
-              target='_blank'
-              rel='noreferrer'
-            >
-              <div className='rounded-full shadow-lg bg-[#5FCEBA] shadow-gray-900 p-6 cursor-pointer hover:scale-110 ease-in duration-300 mr-4'>
-                <FaGithub />
-              </div>
-            </Link>
-            <Link
-              href='https://twitter.com/lastic_xyz'
-              target='_blank'
-              rel='noreferrer'
-            >
-              <div className='rounded-full shadow-lg bg-[#FF977F] shadow-gray-900 p-6 cursor-pointer hover:scale-105 ease-in duration-300 mr-4'>
-                <FaTwitter />
-              </div>
-            </Link>
-            <Link
-              href='https://medium.com/lastic-marketplace'
-              target='_blank'
-              rel='noreferrer'
-            >
-              <div className='rounded-full shadow-lg bg-[#FFD28A] shadow-gray-900 p-6 cursor-pointer hover:scale-110 ease-in duration-300 mr-4'>
-                <FaMedium />
-              </div>
-            </Link>
-            <Link
-              href='https://t.me/+khw2i6GGYFw3NDNi'
-              target='_blank'
-              rel='noreferrer'
-            >
-              <div className='rounded-full shadow-lg bg-[#76EDDD] shadow-gray-900 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
-                <FaTelegram />
-              </div>
-            </Link>
-          </div>
+          <h2 className='py-4 text-white text-5xl font-syncopate md:px-0 text-gray-800'>
+            the world&apos;s first blockspace marketplace.
+          </h2>
+          <h4 className='py-4 md:px-0 text-teal-1 text-2xl md:text-2xl mx-auto'>
+          Do anything you want with your blockspace
+          </h4>
+          <PrimaryButton title='Launch marketplace' location='/instacore'/>
         </div>
       </div>
 
@@ -68,7 +29,48 @@ const Hero = () => {
           objectFit="cover"
         />
       </div>
+
+      
     </div>
+              <div className='flex items-center justify-center md:justify-start max-w-[400px] px-4 py-8 mx-auto'>
+              <Link
+                href='https://github.com/LasticXYZ/Lastic'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <div className='rounded-full shadow-lg bg-[#5FCEBA] shadow-gray-900 p-6 cursor-pointer hover:scale-110 ease-in duration-300 mr-4'>
+                  <FaGithub />
+                </div>
+              </Link>
+              <Link
+                href='https://twitter.com/lastic_xyz'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <div className='rounded-full shadow-lg bg-[#FF977F] shadow-gray-900 p-6 cursor-pointer hover:scale-105 ease-in duration-300 mr-4'>
+                  <FaTwitter />
+                </div>
+              </Link>
+              <Link
+                href='https://medium.com/lastic-marketplace'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <div className='rounded-full shadow-lg bg-[#FFD28A] shadow-gray-900 p-6 cursor-pointer hover:scale-110 ease-in duration-300 mr-4'>
+                  <FaMedium />
+                </div>
+              </Link>
+              <Link
+                href='https://t.me/+khw2i6GGYFw3NDNi'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <div className='rounded-full shadow-lg bg-[#76EDDD] shadow-gray-900 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                  <FaTelegram />
+                </div>
+              </Link>
+            </div>
+        </div>
   );
 };
 
