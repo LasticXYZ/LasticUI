b87a58ecd56ca2d22f1163bd31ac5b3e1ce19659
Aurora Poppyseed <aurora.makovac@gmail.com>
2023-11-03 16:45:12 +0100

    more responsive

--- static_frontend/src/app/(Website)/BlockspaceMarketplace.tsx
+++ static_frontend/src/app/(Website)/BlockspaceMarketplace.tsx
@@ -3,45 +3,46 @@ import React from 'react';
 
 const BlockspaceMarketplace = () => {
     return (
-        <div className="bg-pink-1 border bg-opacity-70 border-gray-9">
-        <section className="mx-auto max-w-9xl py-28 px-4 sm:px-6 lg:px-8">
-            <h2 className="font-syncopate text-center font-bold pt-14 px-5 text-2xl md:text-1xl xl:text-2xl">
-            A Blockspace Marketplace?
-            </h2>
+        <div className="bg-pink-1 pt-10 border-y bg-opacity-70 border-gray-9">
+            <section className="mx-auto max-w-9xl py-12 px-4 sm:px-6 lg:px-8">
+                <h2 className="font-syncopate text-center font-bold pt-2 sm:pt-14 px-5 text-xl sm:text-xl md:text-2xl">
+                    A Blockspace Marketplace?
+                </h2>
 
-            <div className="grid grid-cols-2 items-center justify-between py-20 ">
-                <div className="flex-2 pr-8">
-                    <Image 
-                    src="/assets/Images/deeper_dive.png" 
-                    alt="Abstract Art" 
-                    className="w-full rounded-2xl boreder border-gray-9" 
-                    width={400}
-                    height={400}
-                    />
+                <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-between gap-4 py-12">
+                    <div className="md:flex-1">
+                        <div className="relative h-72 md:h-96 xl:h-[500px] w-full">
+                            <Image 
+                                src="/assets/Images/deeper_dive.png" 
+                                alt="Abstract Art" 
+                                className="rounded-2xl border border-gray-9 object-cover" 
+                                layout="fill"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="text-sm px-4 md:text-base md:flex-1">
+                        <p className="my-4 text-base py-4 sm:text-lg font-bold text-gray-600">
+                            Lastic is a user-friendly marketplace for buying and selling blockspace tailored to your needs, built on Polkadot Coretime.
+                        </p>
+
+                        <ul className='list-disc pl-5 sm:pl-8'>
+                            <li className="py-3">
+                                <strong className="font-semibold">Buy or Sell Blockspace: </strong> 
+                                One place for your blockspace.
+                            </li>
+                            <li className="py-3">
+                                <strong className="font-semibold">Use your blockspace: </strong>
+                                Connect your Polkadot parachain, Wasm smart contract, ZK Prover, or any other computation.
+                            </li>
+                            <li className="py-3">
+                                <strong className="font-semibold">Automation and renewals: </strong> 
+                                Allow parachains to automate their coretime allocation based on current market conditions.
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-
-                <div className="flex-3 pl-16">
-                    <p className="my-4 font-bold text-gray-600">
-                        Lastic is a user-friendly marketplace for buying and selling blockspace tailored to your needs, built on Polkadot Coretime.
-                    </p>
-
-                    <ul className='py-2 px-4'>
-                        <li className="mb-2 py-3">
-                        <strong className="font-semibold">1. Buy or Sell Blockspace: </strong> 
-                            One place for your blockspace.
-                        </li>
-                        <li className="mb-2 py-3">
-                            <strong className="font-semibold">2. Use your blockspace: </strong>
-                            Connect your Polkadot parachain, Wasm smart contract, ZK Prover, or any other computation.
-                        </li>
-                        <li className="mb-2 py-3">
-                            <strong className="font-semibold">3. Automation and renewals: </strong> 
-                            Allow parachains to automate their coretime allocation based on current market conditions.
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </section>
+            </section>
         </div>
     );
 };
--- static_frontend/src/app/(Website)/Hero.tsx
+++ static_frontend/src/app/(Website)/Hero.tsx
@@ -8,24 +8,25 @@ import PrimaryButton from '@/components/button/PrimaryButton';
 const Hero = () => {
   return (
     <div id='home' className="h-screen w-screen flex justify-center items-center">
-      <div className='flex flex-col md:flex-row xl:justify-between items-center w-full max-w-7xl px-4 md:px-0'>
+      <div className='flex flex-col lg:flex-row xl:justify-between items-center w-full max-w-7xl px-4 md:px-0'>
 
       {/* Text Section */}
-      <div className='w-full md:max-w-lg text-center md:text-left mf:px-5 lg:max-w-2xl py-20 mx-auto p-2 relative z-10'>
+      <div className='w-full md:max-w-lg text-center lg:text-left md:px-5 lg:max-w-2xl pt-15 pb-4 md:py-20 mx-auto p-2 relative z-10'>
         <div>
-          <h2 className='pt-20 py-4 text-gray-17 text-3xl sm:4xl xl:text-5xl font-syncopate font-bold'>
-            the world&apos;s first  
-            <span className='leading-relaxed px-3 text-transparent bg-clip-text bg-gradient-to-r from-lastic-spectrum-from via-lastic-spectrum-via to-lastic-spectrum-to'>blockspace</span> marketplace.
+          <h2 className='leading-normal lg:leading-snug pt-20 py-4 text-gray-17 text-2xl sm:text-4xl lg:text-5xl font-syncopate font-bold'>
+            the world&apos;s first 
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-lastic-spectrum-from via-lastic-spectrum-via to-lastic-spectrum-to'> blockspace </span>
+             marketplace.
           </h2>
           <p className='py-4 text-montserrat xl:max-w-md text-gray-8 text-lg xl:text-xl'>
           Do anything you want with your blockspace
           </p>
-          <div className='pt-8 xl:pt-10 flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4'>
+          <div className='pt-8 xl:pt-10 flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-2'>
               <PrimaryButton title='Launch' location='/instacore'/>
               <SecondaryButton title='See Whitepaper' location='/instacore'/>
           </div>
         </div>
-        <div className='flex items-center justify-center md:justify-start pt-8 xl:pt-10 space-x-4'>
+        <div className='flex items-center justify-center lg:justify-start pt-8 xl:pt-10 space-x-4'>
           <Link
             href='https://github.com/LasticXYZ/Lastic'
             target='_blank'
@@ -40,7 +41,7 @@ const Hero = () => {
             target='_blank'
             rel='noreferrer'
             legacyBehavior>
-            <div className='rounded-full border border-gray-9 shadow-gray-900 p-4 cursor-pointer hover:scale-110 ease-in duration-300 mr-4'>
+            <div className='rounded-full border border-gray-9 shadow-gray-900 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
               <FaTwitter />
             </div>
           </Link>
@@ -49,7 +50,7 @@ const Hero = () => {
             target='_blank'
             rel='noreferrer'
             legacyBehavior>
-            <div className='rounded-full border border-gray-9 shadow-gray-900 p-4 cursor-pointer hover:scale-110 ease-in duration-300 mr-4'>
+            <div className='rounded-full border border-gray-9 shadow-gray-900 p-4 cursor-pointer hover:scale-110 ease-in duration-300 '>
               <FaMedium />
             </div>
           </Link>
@@ -58,7 +59,7 @@ const Hero = () => {
             target='_blank'
             rel='noreferrer'
             legacyBehavior>
-            <div className='rounded-full border border-gray-9 shadow-gray-900 p-4 cursor-pointer hover:scale-110 ease-in duration-300 mr-4'>
+            <div className='rounded-full border border-gray-9 shadow-gray-900 p-4 cursor-pointer hover:scale-110 ease-in duration-300 '>
               <FaTelegram />
             </div>
           </Link>
@@ -66,13 +67,14 @@ const Hero = () => {
       </div>
 
         {/* Image Section */}
-        <div className="w-11/12 xl:w-1/2 mx-auto md:mx-0 relative z-10">
+        <div className="w-11/12 xl:w-1/2 justify-center mx-auto relative z-10">
           <Image
             src='/assets/Images/hero-page.png'
             alt='hero'
             width={700}
             height={700}
             quality={100}
+            className='justify-center mx-auto'
           />
         </div>
       </div>
