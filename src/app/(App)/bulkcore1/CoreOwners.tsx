eb68fe64aff62bdfc39e1ebe93f682c5ceeeeaca
aurora <aurora.makovac@gmail.com>
2024-02-12 13:39:31 +0100

    able to see past transactions, let's now limit how many there are fetched

--- src/app/(App)/bulkcore1/CoreOwners.tsx
+++ src/app/(App)/bulkcore1/CoreOwners.tsx
@@ -1,83 +1,69 @@
-import GeneralTable from '@/components/table/GeneralTable'
-import TagComp from '@/components/tags/TagComp'
-import React, { useState } from 'react'
+import GeneralTable from '@/components/table/GeneralTable';
+import { parseNativeTokenToHuman, toShortAddress } from '@/utils/account/token';
+import { useBalance, useInkathon } from '@poppyseed/lastic-sdk';
+import { GraphLike, PurchasedEvent, getClient } from '@poppyseed/squid-sdk';
+import { format } from 'date-fns';
+import { useEffect, useMemo, useState } from 'react';
 
-const PoolsSection = () => {
+const PastTransactions = () => {
+  const { activeAccount } = useInkathon();
+
+  const [result, setResult] = useState<GraphLike<PurchasedEvent[]> | null>(null);
+  const client = getClient();
+
+  let { tokenSymbol } = useBalance(activeAccount?.address, true)
+  tokenSymbol = tokenSymbol || 'UNIT';
+
+  const query = client.eventAllPurchased();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedResult: GraphLike<PurchasedEvent[]> = await client.fetch(query);
+      setResult(fetchedResult);
+    };
+    
+    fetchData();
+  }, []);
+
+  const reversedData = useMemo(() => {
+    // Make a copy of the event array (if it exists) and reverse the copy
+    return [...(result?.data.event || [])].reverse();
+  }, [result]);
+
   const TableHeader = [
-    { title: '#' },
-    { title: 'Para ID' },
-    { title: 'Project Name' },
-    { title: 'Owner' },
-    { title: 'Cores Owned' },
-    { title: '% Owned' },
-    { title: 'Core Type' },
-    { title: 'Period Until Renewal' },
-  ]
+    { title: 'Time' },
+    { title: 'Block Number' },
+    { title: 'Purchased By'},
+    { title: 'Core Nb.' },
+    { title: 'RegionID Begin' },
+    { title: 'Price' },
+  ];
 
-  const TableData = [
-    {
-      href: '/',
-      data: [
-        '1.',
-        '200',
-        'Asset Hub',
-        '0x302...1231',
-        <TagComp key="tag-1" className="mx-4 my-2" title="2.12" />,
-        ' 0.21%',
-        'Instantanious',
-        '/',
-      ],
-    },
-    {
-      href: '/',
-      data: [
-        '2.',
-        '201',
-        'Asset Hub',
-        '0x303...1232',
-        <TagComp key="tag-2" className="mx-4 my-2" title="3.45" />,
-        ' 0.33%',
-        'Instantanious',
-        '/',
-      ],
-    },
-    {
-      href: '/',
-      data: [
-        '3.',
-        '202',
-        'Asset Hub',
-        '0x304...1233',
-        <TagComp key="tag-3" className="mx-4 my-2" title="5.67" />,
-        ' 0.45%',
-        'Instantanious',
-        '/',
-      ],
-    },
-    {
-      href: '/',
-      data: [
-        '4.',
-        '203',
-        'Asset Hub',
-        '0x305...1234',
-        <TagComp key="tag-4" className="mx-4 my-2" title="7.89" />,
-        ' 0.78%',
-        'Instantanious',
-        '/',
-      ],
-    },
-  ]
+  // Transform result into table data
+  const TableData = reversedData.map((event, index) => ({
+    data: [
+      event.timestamp ? format(new Date(event.timestamp), 'MMMM dd, yyyy HH:mm:ss OOOO') : '',
+      event.blockNumber?.toString(),
+      toShortAddress(event.who, 5),
+      event.regionId.core?.toString(),
+      event.regionId.begin?.toString(),
+      `${parseNativeTokenToHuman({paid: event.price?.toString(), decimals: 12})} ${tokenSymbol}`,
+    ],
+  })) || [];
 
   return (
-    <>
-      <div className="mx-auto max-w-9xl px-4 mt-5 sm:px-6 lg:px-8">
-        <div>
-          <GeneralTable tableData={TableData} tableHeader={TableHeader} colClass="grid-cols-8" />
+    <div className="">
+        <div className="mx-auto max-w-9xl px-4 sm:px-6 lg:px-8">
+          <div>
+            {(result) ? (
+              <GeneralTable tableData={TableData} tableHeader={TableHeader} colClass="grid-cols-6" />
+            ) : (
+              <p>Loading transactions...</p>
+            )}
+          </div>
         </div>
-      </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default PoolsSection
+export default PastTransactions;
