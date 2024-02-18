618e1dafc4864ae91eeaa50017398838ffb2c44c
Aurora Poppyseed <aurora.makovac@gmail.com>
2024-02-18 15:48:53 +0100

    adding visuals

--- src/app/(App)/teleport/page.tsx
+++ src/app/(App)/teleport/page.tsx
@@ -3,6 +3,7 @@
 import Border from '@/components/border/Border'
 import WalletStatus from '@/components/walletStatus/WalletStatus'
 import { toShortAddress } from '@/utils/account/token'
+import { ArrowPathIcon } from '@heroicons/react/24/outline'
 import { DispatchError, Hash } from '@polkadot/types/interfaces'
 import { ISubmittableResult } from '@polkadot/types/types'
 import { useBalance, useInkathon } from '@poppyseed/lastic-sdk'
@@ -13,7 +14,6 @@ import { useState } from 'react'
 
 export type TxCbOnSuccessParams = { blockHash: Hash; txHash: Hash }
 
-
 const handleTransaction = ({
   onSuccess,
   onError,
@@ -23,129 +23,87 @@ const handleTransaction = ({
   onError: (err: DispatchError) => void,
   onResult?: (result: ISubmittableResult) => void
 }) => (result: ISubmittableResult): void => {
-  onResult(result);
+  onResult(result)
   if (result.dispatchError) {
-    console.warn('[EXEC] dispatchError', result);
-    onError(result.dispatchError);
-    return;
+    console.warn('[EXEC] dispatchError', result)
+    onError(result.dispatchError)
+    return
   }
 
   if (result.status.isFinalized) {
-    console.log('[EXEC] Finalized', result);
-    console.log(`[EXEC] blockHash ${result.status.asFinalized}`);
-    onSuccess({ blockHash: result.status.asFinalized, txHash: result.txHash });
+    console.log('[EXEC] Finalized', result)
+    console.log(`[EXEC] blockHash ${result.status.asFinalized}`)
+    onSuccess({ blockHash: result.status.asFinalized, txHash: result.txHash })
   }
-};
+}
 
 export const notificationTypes = {
   success: 'success',
   info: 'info',
   danger: 'danger',
   warn: 'warning',
-};
-
-export const showNotification = (
-  message: string,
-  type: keyof typeof notificationTypes = 'info',
-): void => {
-  const duration = type === 'danger' ? 15000 : 10000;
-  console.log(`[${type.toUpperCase()}] ${message}`, `Duration: ${duration}ms`);
-};
-
-type ChainOptions = {
-  [key: string]: string; // Adding an index signature
-};
-
-const chainOptions: ChainOptions = {
-  'Rococo Relay Chain': '/assets/Images/NetworkIcons/rococo-img.svg',
-  'Rococo Coretime Testnet': '/assets/Images/NetworkIcons/assethub.svg',
 }
 
+export const showNotification = (message: string, type: keyof typeof notificationTypes = 'info') => {
+  const duration = type === 'danger' ? 15000 : 10000
+  console.log(`[${type.toUpperCase()}] ${message}`, `Duration: ${duration}ms`)
+}
+
+const chainOptions: { [key: string]: string } = {
+  'Rococo Relay Chain': '/assets/Images/NetworkIcons/rococo-img.svg',
+  'Rococo Coretime Testnet': '/assets/Images/NetworkIcons/coretime-img.svg',
+}
 
 const Teleport = () => {
-  const [amount, setAmount] = useState(NaN)
-  const [isLoading, setIsLoading] = useState(false)
+  const [amount, setAmount] = useState<number>(0)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [isRelayToPara, setIsRelayToPara] = useState<boolean>(true) // State to toggle direction
   const { api, relayApi, activeAccount, activeChain, activeRelayChain, activeSigner } = useInkathon()
 
-  const { balanceFormatted, balance, tokenSymbol, tokenDecimals } = useBalance(activeAccount?.address, true, {
-    forceUnit: false,
-    fixedDecimals: 2,
-    removeTrailingZeros: true,
-  })
+  const { balanceFormatted, balance, tokenSymbol, tokenDecimals } = useBalance(activeAccount?.address, true)
 
   const transactionCallback = handleTransaction({
     onSuccess: ({ blockHash }) => {
-      showNotification(`Transaction finalized at blockHash ${blockHash}`, 'success');
+      showNotification(`Transaction finalized at blockHash ${blockHash}`, 'success')
     },
     onError: (error) => {
-      showNotification(error.toString(), 'danger');
+      showNotification(error instanceof Error ? error.message : String(error), 'danger')
     },
-  });
+  })
 
-  const functionSendXCMRelaytoPara = async (amountToSend: number) => {
+  const functionSendXCM = async (amountToSend: number) => {
     if (!activeAccount || !relayApi || !api) return
 
     setIsLoading(true)
 
     try {
-      const call = await Builder(relayApi)
-        .to('CoretimeKusama')
-        .amount(amountToSend)
-        .address(activeAccount.address)
-        .build();
+      const call = isRelayToPara
+        ? await Builder(relayApi).to('CoretimeKusama').amount(amountToSend).address(activeAccount.address).build()
+        : await Builder(api).from('CoretimeKusama').amount(amountToSend).address(activeAccount.address).build()
 
-      call.signAndSend(activeAccount.address, { signer: activeSigner }, transactionCallback);
+      call.signAndSend(activeAccount.address, { signer: activeSigner }, transactionCallback)
     } catch (error) {
-      console.error('Error in XCM transaction:', error);
-      // Check if error is an instance of Error
-      if (error instanceof Error) {
-        showNotification('Error in XCM transaction: ' + error.message, 'danger');
-      } else {
-        // If it's not an Error instance, you might just log it as a string
-        showNotification('Error in XCM transaction: An unexpected error occurred.', 'danger');
-      }
+      console.error('Error in XCM transaction:', error)
+      showNotification(error instanceof Error ? 'Error in XCM transaction: ' + error.message : 'Error in XCM transaction: An unexpected error occurred.', 'danger')
     } finally {
-      setIsLoading(false);
-    }
-  }
-
-  const functionSendXCMParatoRelay = async (amountToSend: number) => {
-    if (!activeAccount || !relayApi || !api) return
-
-    setIsLoading(true)
-
-    try {
-      const call = await Builder(api)
-        .from('CoretimeKusama')
-        .amount(amountToSend)
-        .address(activeAccount.address)
-        .build();
-
-      call.signAndSend(activeAccount.address, { signer: activeSigner }, transactionCallback);
-    } catch (error) {
-      console.error('Error in XCM transaction:', error);
-      // Check if error is an instance of Error
-      if (error instanceof Error) {
-        showNotification('Error in XCM transaction: ' + error.message, 'danger');
-      } else {
-        // If it's not an Error instance, you might just log it as a string
-        showNotification('Error in XCM transaction: An unexpected error occurred.', 'danger');
-      }
-    } finally {
-      setIsLoading(false);
+      setIsLoading(false)
     }
   }
 
   const handleMaxClick = () => {
-    setAmount(Number(balance)/ 10**tokenDecimals)
+    setAmount(Number(balance) / 10 ** tokenDecimals)
+  }
+
+  const toggleDirection = () => {
+    setIsRelayToPara(!isRelayToPara)
   }
 
   if (!activeAccount) {
     return (
       <section className="mx-auto max-w-9xl py-7 px-4 sm:px-6 lg:px-8">
-      <Border>
-        <WalletStatus customMessage='Connect your wallet to Teleport your assets'/>
-      </Border>
+        <Border>
+          <WalletStatus customMessage="Connect your wallet to Teleport your assets" />
+        </Border>
       </section>
     )
   }
@@ -153,7 +111,7 @@ const Teleport = () => {
   return (
     <section className="mx-auto max-w-9xl py-7 px-4 sm:px-6 lg:px-8">
       <Border>
-        <div className="container mx-auto p-6">
+      <div className="container mx-auto p-6">
           <h1 className="text-2xl font-bold mb-4 font-syncopate">Teleport</h1>
           <p className="mb-6">
             Teleport assets between networks in the Polkadot and Kusama Ecosystem.
@@ -163,41 +121,49 @@ const Teleport = () => {
           </Link>
           <hr className="my-4 border-gray-9" />
 
-          <div className="grid grid-cols-2 gap-6 mb-6">
-            <div>
+          <div className="flex flex-row items-center justify-between w-full gap-6 mb-6">
+            <div className='w-full'>
               <p className="mb-2">Source chain</p>
               <div className="relative w-full rounded-lg bg-opacity-20 py-2 pl-3 pr-10 text-left border border-gray-9 focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
                 <span className="flex items-center">
                   <Image
-                    src={chainOptions[activeRelayChain?.name || 'Rococo Relay Chain']}
+                    src={chainOptions[(isRelayToPara ? activeRelayChain?.name : activeChain?.name) || 'Rococo Relay Chain']}
                     alt=""
                     width='0'
                     height="0"
                     style={{ width: '2em', height: 'auto' }}
                     className="flex-shrink-0 h-6 w-6 rounded-full"
                   />
-                  <span className="ml-3 block truncate">{activeRelayChain?.name}</span>
+                  <span className="ml-3 block truncate">{isRelayToPara ? activeRelayChain?.name : activeChain?.name}</span>
                 </span>
               </div>
             </div>
-            <div>
+            <div className='pt-8'>
+              <button onClick={toggleDirection} className="">
+                <ArrowPathIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div className='w-full'>
               <p className="mb-2">Destination</p>
               <div className="relative w-full rounded-lg bg-opacity-20 py-2 pl-3 pr-10 text-left border border-gray-9 focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
                 <span className="flex items-center">
                   <Image
-                    src={chainOptions[activeChain?.name || 'Rococo Relay Chain']}
+                    src={chainOptions[(isRelayToPara ? activeChain?.name : activeRelayChain?.name) || 'Rococo Relay Chain']}
                     alt=""
                     width='0'
                     height="0"
                     style={{ width: '2em', height: 'auto' }}
                     className="flex-shrink-0 h-6 w-6 rounded-full"
                   />
-                  <span className="ml-3 block truncate">{activeChain?.name}</span>
+                  <span className="ml-3 block truncate">{isRelayToPara ? activeChain?.name : activeRelayChain?.name}</span>
                 </span>
               </div>
             </div>
           </div>
 
+          <button onClick={toggleDirection} className="mb-4 py-2 px-4 bg-blue-500 text-black rounded hover:bg-blue-600 transition">
+            Move assets from {isRelayToPara ? 'Relay to Parachain' : 'Parachain to Relay'}
+          </button>
           <div className="mb-6">
             <p className="mb-2">Asset Amount</p>
             <div className="flex items-center p">
@@ -216,15 +182,13 @@ const Teleport = () => {
               <button onClick={handleMaxClick} className="text-blue-500">Max</button>
             </div>
           </div>
-
           <button
-            onClick={() => functionSendXCMParatoRelay(amount * 10 ** tokenDecimals)}
-            disabled={isLoading} // Disable button when operation is in progress
+            onClick={() => functionSendXCM(amount * 10 ** tokenDecimals)}
+            disabled={isLoading}
             className={`w-full py-2 border border-gray-9 rounded-lg hover:bg-gray-3 ${isLoading ? 'bg-gray-10' : ''}`}
           >
             {isLoading ? 'Processing...' : 'Proceed To Confirmation'}
           </button>
-
           <p className="mt-6">
             You will receive {amount || 0} {tokenSymbol} on {activeChain?.name} to {toShortAddress(activeAccount?.address, 4)}
           </p>
