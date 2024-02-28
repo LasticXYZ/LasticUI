293c39b50399f4a37b887bdd69f9048317c8d6fa
Noah Joeris <noahjoeris@gmail.com>
2024-02-28 19:18:57 +0100

    change layout and fix responsive issues

--- src/app/(App)/core/[number]/[regionId]/Core.tsx
+++ src/app/(App)/core/[number]/[regionId]/Core.tsx
@@ -168,10 +168,11 @@ export default function BrokerRegionData({
           </div>
         </div>
       </Border>
+
       {/* Time Section */}
       <section className="mt-8">
         <Border>
-          <div className=" p-10">
+          <div className="p-10">
             <div>
               <div className="flex justify-between rounded-full mx-10 bg-pink-4 px-16 py-10 bg-opacity-30 items-center my-6">
                 <div className="text-xl font-bold font-unbounded uppercase text-gray-21">
@@ -182,130 +183,129 @@ export default function BrokerRegionData({
                 </div>
               </div>
             </div>
+
             <TimelineComponent
               currentBlockNumber={currentBlockNumber}
               saleInfo={saleInfo}
               config={configuration}
               constants={brokerConstants}
             />
+
             <div className="pt-5 pl-10">
               <h3 className="text-xl font-unbounded uppercase font-bold">Utilization</h3>
             </div>
+
             <TimelineUtilizeCore
               currentRelayBlock={currentRelayBlock}
               beginRegion={regionId}
               config={configuration}
               constants={brokerConstants}
             />
+
             <div className="pt-5 pl-10">
               <h3 className="text-xl font-unbounded uppercase font-bold">Note</h3>
             </div>
-            <div className="flex flex-row justify-between">
-              {/* Disabled conditional rendering for testing purposes. Enable later again */}
-              {/* {region.owner.owner === activeAccount.address ? ( */}
-              <>
-                <div className="flex flex-col italic max-w-md text-gray-12 items-start justify-center px-4 py-8">
-                  Note: This core is yours. You are able to:
-                  <ul className="px-2 py-2">
-                    <li> * Transfer your Core - transfer is to another account</li>
-                    <li>
-                      {' '}
-                      * Utilize it - Make sure to have your Parachain ready in order to test it out.
-                    </li>
-                    <li> * Split it up</li>
-                    <li> * Recombine it</li>
-                    <li> * Assign it to a task</li>
-                  </ul>
-                </div>
 
-                <div className="grid grid-cols-2 gap-4 py-10">
-                  {/* Modals*/}
-                  <TransferModal
-                    isOpen={isTransferModalOpen}
-                    onClose={() => setIsTransferModalOpen(false)}
-                    regionId={{
-                      begin: region.detail[0].begin.replace(/,/g, ''),
-                      core: region.detail[0].core,
-                      mask: region.detail[0].mask,
-                    }}
-                  />
-
-                  <AssignModal
-                    isOpen={isAssignModalOpen}
-                    onClose={() => setIsAssignModalOpen(false)}
-                    regionId={{
-                      begin: region.detail[0].begin.replace(/,/g, ''),
-                      core: region.detail[0].core,
-                      mask: region.detail[0].mask,
-                    }}
-                  />
-
-                  <PartitionCoreModal
-                    isOpen={isPartitionModalOpen}
-                    onClose={() => setIsPartitionModalOpen(false)}
-                    regionId={{
-                      begin: region.detail[0].begin.replace(/,/g, ''),
-                      core: region.detail[0].core,
-                      mask: region.detail[0].mask,
-                    }}
-                  />
-
-                  <InterlaceCoreModal
-                    isOpen={isInterlaceModalOpen}
-                    onClose={() => setIsInterlaceModalOpen(false)}
-                    regionId={{
-                      begin: region.detail[0].begin.replace(/,/g, ''),
-                      core: region.detail[0].core,
-                      mask: region.detail[0].mask,
-                    }}
-                  />
-
-                  {/* Buttons*/}
-                  <div className="text-2xl font-bold font-unbounded uppercase text-gray-21">
-                    <SecondaryButton
-                      title="Transfer Core"
-                      onClick={() => setIsTransferModalOpen(true)}
-                    />
+            <div className="flex flex-row flex-wrap justify-between">
+              {region.owner.owner === activeAccount.address ? (
+                <>
+                  <div className="flex flex-col italic max-w-md text-gray-12 items-start justify-center px-4 py-8">
+                    This core is yours. You are able to:
+                    <ul className="px-2 py-2">
+                      <li> * Transfer your core to another account</li>
+                      <li> * Utilize it for a parachain</li>
+                      <li> * Split it up</li>
+                      <li> * Change block production frequency</li>
+                      <li> * Assign it to a task</li>
+                    </ul>
                   </div>
 
-                  <div className="text-2xl font-bold font-unbounded uppercase text-gray-21">
-                    <SecondaryButton
-                      title="Assign Core"
-                      onClick={() => setIsAssignModalOpen(true)}
+                  <div className="grid grid-cols-1 lg:grid-cols-2 gap-3 py-10">
+                    {/* Modals*/}
+                    <TransferModal
+                      isOpen={isTransferModalOpen}
+                      onClose={() => setIsTransferModalOpen(false)}
+                      regionId={{
+                        begin: region.detail[0].begin.replace(/,/g, ''),
+                        core: region.detail[0].core,
+                        mask: region.detail[0].mask,
+                      }}
                     />
-                  </div>
 
-                  <div className="text-2xl font-bold font-unbounded uppercase text-gray-21">
-                    <SecondaryButton
-                      title="Interlace Core"
-                      onClick={() => setIsInterlaceModalOpen(true)}
-                      disabled={true}
+                    <AssignModal
+                      isOpen={isAssignModalOpen}
+                      onClose={() => setIsAssignModalOpen(false)}
+                      regionId={{
+                        begin: region.detail[0].begin.replace(/,/g, ''),
+                        core: region.detail[0].core,
+                        mask: region.detail[0].mask,
+                      }}
                     />
-                  </div>
 
-                  <div className="text-2xl font-bold font-unbounded uppercase text-gray-21">
-                    <SecondaryButton
-                      title="Split Core"
-                      onClick={() => setIsPartitionModalOpen(true)}
-                      disabled={true}
+                    <PartitionCoreModal
+                      isOpen={isPartitionModalOpen}
+                      onClose={() => setIsPartitionModalOpen(false)}
+                      regionId={{
+                        begin: region.detail[0].begin.replace(/,/g, ''),
+                        core: region.detail[0].core,
+                        mask: region.detail[0].mask,
+                      }}
                     />
-                  </div>
 
-                  <div className="text-2xl font-bold font-unbounded uppercase text-gray-21">
-                    <SecondaryButton
-                      title="Combine core"
-                      onClick={() => console.log('Combine')}
-                      disabled={true}
+                    <InterlaceCoreModal
+                      isOpen={isInterlaceModalOpen}
+                      onClose={() => setIsInterlaceModalOpen(false)}
+                      regionId={{
+                        begin: region.detail[0].begin.replace(/,/g, ''),
+                        core: region.detail[0].core,
+                        mask: region.detail[0].mask,
+                      }}
                     />
+
+                    {/* Buttons*/}
+                    <div className="text-2xl font-bold font-unbounded uppercase text-gray-21">
+                      <SecondaryButton
+                        title="Transfer Core"
+                        onClick={() => setIsTransferModalOpen(true)}
+                      />
+                    </div>
+
+                    <div className="text-2xl font-bold font-unbounded uppercase text-gray-21">
+                      <SecondaryButton
+                        title="Assign Core"
+                        onClick={() => setIsAssignModalOpen(true)}
+                      />
+                    </div>
+
+                    <div className="text-2xl font-bold font-unbounded uppercase text-gray-21">
+                      <SecondaryButton
+                        title="Interlace Core"
+                        onClick={() => setIsInterlaceModalOpen(true)}
+                      />
+                    </div>
+
+                    <div className="text-2xl font-bold font-unbounded uppercase text-gray-21">
+                      <SecondaryButton
+                        title="Split Core"
+                        onClick={() => setIsPartitionModalOpen(true)}
+                      />
+                    </div>
+
+                    <div className="text-2xl font-bold font-unbounded uppercase text-gray-21">
+                      <SecondaryButton
+                        title="Combine core"
+                        onClick={() => console.log('Combine')}
+                        disabled={true}
+                      />
+                    </div>
                   </div>
-                </div>
-              </>
-              {/*  ) : (
+                </>
+              ) : (
                 <div className="flex flex-col italic max-w-3xl text-gray-12 items-center justify-center px-2 py-8">
                   Note: You do not own this core. After buying a core you will be able to: Transfer
                   it, Utilize it, Split it up and Recombine it.
                 </div>
-              )}  */}
+              )}
             </div>
           </div>
         </Border>
