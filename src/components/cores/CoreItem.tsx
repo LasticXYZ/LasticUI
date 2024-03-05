9c7d9235f7d435e80148abdffa5d74853cc94344
arjanjohan <arjanvanhoogdalem@gmail.com>
2024-03-05 15:59:45 +0800

    add interlace/partition tag on mycores

--- src/components/cores/CoreItem.tsx
+++ src/components/cores/CoreItem.tsx
@@ -31,11 +31,23 @@ const Card: React.FC<CardProps> = ({
   return (
     <Border className="px-10 py-6 hover:bg-pink-1 hover:cursor-pointer">
       <Link href={`/core/${coreNumber}/${beginStr}`}>
-        <div>
-          <div className="uppercase font-unbounded uppercase tracking-wide text-md text-indigo-500 font-semibold">
-            Core Nb. {coreNumber}
-          </div>
+      <div>
+  <div className="uppercase font-unbounded uppercase tracking-wide text-md text-indigo-500 font-semibold flex justify-between items-center">
+    <span>Core Nb. {coreNumber}</span>
+    <div className="flex space-x-2"> {/* Container to hold both buttons next to each other */}
+      {parseInt(end) - parseInt(begin) < 1260 && (
+        <div className="bg-pink-3 border border-gray-8 px-2 py-1 text-xs font-semibold uppercase rounded-full shadow-lg">
+          Partitioned
         </div>
+      )}
+      {mask !== '0xffffffffffffffffffff' && (
+        <div className="bg-pink-3 border border-gray-8 px-2 py-1 text-xs font-semibold uppercase rounded-full shadow-lg">
+          Interlaced
+        </div>
+      )}
+    </div>
+  </div>
+  </div>
         <div className="mx-auto pt-4 flex flex-row items-center justify-between overflow-hidden">
           <div className="px-5">
             <Image src="/assets/Images/core1.png" alt="Lastic Logo" width={130} height={100} />
