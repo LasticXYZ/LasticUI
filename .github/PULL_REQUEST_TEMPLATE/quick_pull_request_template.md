d733fb37c6dd9d6714177f91e545c2dbb09555c7
Aurora Poppyseed <aurora.makovac@gmail.com>
2024-03-24 13:56:32 +0100

    adding pull request template

---
+++ .github/PULL_REQUEST_TEMPLATE/quick_pull_request_template.md
@@ -0,0 +1,18 @@
+**Thank you for your contribution** to the [Lastic - Blockspace markeplace](https://lastic.xyz).
+
+👇 __ Let's make a quick check before the contribution.
+
+## PR Type
+
+- [ ] Bugfix
+- [ ] Feature
+- [ ] Refactoring
+
+## Context
+
+- [ ] Closes #<issue_number>
+- [ ] Requires deployment <worker/stick/speck>
+
+#### Did your issue had any of the "$" label on it?
+
+- [ ] My BTC address: 
--- src/app/(App)/bulkcore1/CoreUtilisation.tsx
+++ src/app/(App)/bulkcore1/CoreUtilisation.tsx
@@ -1,12 +1,28 @@
 import AccordionTile from '@/components/accordion/AccordionTile'
 import Border from '@/components/border/Border'
 import MiniBarGraph from '@/components/graph/MiniBarGraph'
-import React from 'react'
+import { GraphLike, SaleInitializedEvent, getClient } from '@poppyseed/squid-sdk'
+import React, { useEffect, useState } from 'react'
 import CoreOwners from './CoreOwners'
 
 type CoreUtilisationProps = {}
 
 const CoreUtilisation: React.FC<CoreUtilisationProps> = () => {
+  const [result, setResult] = useState<GraphLike<SaleInitializedEvent[]> | null>(null)
+  const client = getClient()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let query = client.eventAllSaleInitialized()
+      const fetchedResult: GraphLike<SaleInitializedEvent[]> = await client.fetch(query)
+      setResult(fetchedResult)
+    }
+
+    fetchData()
+  }, [client]) // Add offset to the dependency array
+
+  console.log(result)
+
   return (
     <section className="mx-auto max-w-9xl px-4 sm:px-6 lg:px-8">
       <Border>
@@ -17,13 +33,6 @@ const CoreUtilisation: React.FC<CoreUtilisationProps> = () => {
           <div className="col-span-1 grid grid-cols-1 gap-4 mb-4">
             <div className=" p-2">
               <AccordionTile question="Core Utilization" answer="twr-aerawerae" />
-              <AccordionTile question="Project Name" answer="twr-aerawerae" />
-              <AccordionTile question="Para ID" answer="twr-aerawerae" />
-              <AccordionTile question="Nb. of Cores Owned" answer="twr-aerawerae" />
-              <AccordionTile question="% Owned<" answer="twr-aerawerae" />
-              <AccordionTile question="Period Until Renewal" answer="twr-aerawerae" />
-              <AccordionTile question="Monthly price per Core" answer="twr-aerawerae" />
-              <AccordionTile question="Volume and Price" answer="twr-aerawerae" />
             </div>
           </div>
           <div className=" col-span-3 p-4">
