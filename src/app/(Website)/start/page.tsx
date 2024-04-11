1903724994f2e36f76d7a3b6ceaec2201a017059
Aurora Poppyseed <aurora.makovac@gmail.com>
2024-04-11 13:34:20 +0200

    adding pictures for start

--- src/app/(Website)/start/page.tsx
+++ src/app/(Website)/start/page.tsx
@@ -7,84 +7,74 @@ import Image from 'next/image'
 
 const StartPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 my-10">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 my-2">
       {/* Container for the boxes */}
-      <div className="grid grid-cols-1 mt-16 sm:grid-cols-2 gap-8 max-w-6xl mx-auto px-4 py-8 sm:p-4">
+      <div className="grid grid-cols-1 mt-5 sm:grid-cols-2 gap-8 max-w-6xl mx-auto px-4 py-4 sm:p-4">
         {/* EVM Solution Box */}
-        <div>
-          <BorderBlack>
-            <div className="flex flex-col text-white font-bold py-6 px-12 rounded-lg shadow-lg items-center justify-center transition-all duration-300">
-              <Image
-                src="/assets/Images/deeper_dive.png"
-                width={250}
-                height={250}
-                className="rounded-2xl border border-gray-16 "
-                layout="responsive"
-                quality={100}
-                alt="Lastic Logo"
-              />
-              <div className="py-5 px-2 flex flex-col justify-start items-start w-full">
-                <h4 className="font-syne text-3xl py-2">EVM solution</h4>
-                <p className="font-dm_sans text-md text-gray-6 py-2 mb-5">
-                  Explore Lastic&apos;s versatile marketplace, tailored for EVM-compatible Layer One
-                  blockchains. Experience efficiency like never before with services optimized for
-                  Data Availability and Co-Processing.
-                </p>
-                <div className="flex flex-col justify-center items-center w-full">
-                  <div className="p-2">
-                    <PrimaryButtonWeb
-                      title="Coming Soon"
-                      onClick={() => console.log('test')}
-                      disabled={true}
-                    />
-                  </div>
-                  <div className="p-2">
-                    <SecondaryButtonWeb
-                      title="Check the Docs"
-                      location="https://docs.lastic.xyz/"
-                    />
-                  </div>
+        <BorderBlack>
+          <div className="block text-current no-underline">
+            <Image
+              className="w-full h-60 object-cover rounded-t-lg"
+              src="/assets/Images/cool-core3.webp"
+              alt="polkadot solution"
+              width={640}
+              height={360}
+              quality={75}
+              priority={true}
+            />
+            <div className="py-8 px-6 flex flex-col justify-start items-start w-full">
+              <h4 className="font-syne text-3xl py-2">EVM solution</h4>
+              <p className="font-dm_sans text-md text-gray-6 py-2 mb-5">
+                Explore Lastic&apos;s versatile marketplace, tailored for EVM-compatible Layer One
+                blockchains. Experience efficiency like never before with services optimized for
+                Data Availability and Co-Processing.
+              </p>
+              <div className="flex flex-col justify-center py-5 items-center w-full">
+                <div className="p-2">
+                  <PrimaryButtonWeb
+                    title="Coming Soon"
+                    onClick={() => console.log('test')}
+                    disabled={true}
+                  />
+                </div>
+                <div className="p-2">
+                  <SecondaryButtonWeb title="Check the Docs" location="https://docs.lastic.xyz/" />
                 </div>
               </div>
             </div>
-          </BorderBlack>
-        </div>
+          </div>
+        </BorderBlack>
 
         {/* Polkadot Solution Box */}
-        <div>
-          <BorderBlack>
-            <div className="flex flex-col  text-white font-bold py-6 px-12 rounded-lg shadow-lg items-center justify-center transition-all duration-300">
-              <Image
-                src="/assets/Images/deeper_dive.png"
-                width={250}
-                height={250}
-                className="rounded-2xl w-[300px] border border-gray-16 "
-                layout="responsive"
-                quality={100}
-                alt="Lastic Logo"
-              />
-              <div className="py-5 px-2 flex flex-col justify-start items-start w-full">
-                <h4 className="font-syne text-3xl py-2">Polkadot solution</h4>
-                <p className="font-dm_sans text-md text-gray-6 py-2 mb-5">
-                  Join a new era where trading blockspace is as fluid as trading tokens. With
-                  Lastic, your Appchain can leverage on-chain computation and speculative
-                  instruments to maximize efficiency and growth.
-                </p>
-                <div className="flex flex-col justify-center items-center w-full">
-                  <div className="p-2">
-                    <PrimaryButtonWeb title="Go to App" location="/bulkcore1" />
-                  </div>
-                  <div className="p-2">
-                    <SecondaryButtonWeb
-                      title="Check the Docs"
-                      location="https://docs.lastic.xyz/"
-                    />
-                  </div>
+        <BorderBlack>
+          <div className="block text-current no-underline">
+            <Image
+              className="w-full h-60 object-cover rounded-t-lg"
+              src="/assets/Images/cool-core.webp"
+              alt="polkadot solution"
+              width={640}
+              height={360}
+              quality={75}
+              priority={true}
+            />
+            <div className="py-8 px-6 flex flex-col justify-start items-start w-full">
+              <h4 className="font-syne text-3xl py-2">Polkadot solution</h4>
+              <p className="font-dm_sans text-md text-gray-6 py-2 mb-5">
+                Join a new era where trading blockspace is as fluid as trading tokens. With Lastic,
+                your Appchain can leverage on-chain computation and speculative instruments to
+                maximize efficiency and growth.
+              </p>
+              <div className="flex flex-col  py-5 justify-center items-center w-full">
+                <div className="p-2">
+                  <PrimaryButtonWeb title="Go to App" location="/bulkcore1" />
+                </div>
+                <div className="p-2">
+                  <SecondaryButtonWeb title="Check the Docs" location="https://docs.lastic.xyz/" />
                 </div>
               </div>
             </div>
-          </BorderBlack>
-        </div>
+          </div>
+        </BorderBlack>
       </div>
     </div>
   )
