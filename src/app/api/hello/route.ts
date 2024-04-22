eb6644df0db2af5285d6bea8a57a4116b4e9ea54
Noah Joeris <noahjoeris@gmail.com>
2024-04-22 16:26:20 +0200

    try merge

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,10 +1,10 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET (request: NextRequest){
-    const greeting = "Hello World!!"
-    const json = {
-        greeting
-    };
-    
-    return NextResponse.json(json);
+export async function GET(request: NextRequest) {
+  const greeting = 'Hello World!!'
+  const json = {
+    greeting,
+  }
+
+  return NextResponse.json(json)
 }
