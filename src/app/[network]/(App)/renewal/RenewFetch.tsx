ff0eed29a78d9b81cd3a2a1ff74c302cadf0f92d
Aurora Poppyseed <aurora.makovac@gmail.com>
2024-05-22 00:06:55 +0200

    renew core

--- src/app/[network]/(App)/renewal/RenewFetch.tsx
+++ src/app/[network]/(App)/renewal/RenewFetch.tsx
@@ -1,4 +1,6 @@
 import Border from '@/components/border/Border'
+import RenewModal from '@/components/broker/extrinsics/RenewModal'
+import SecondaryButton from '@/components/button/SecondaryButton'
 import GeneralTable from '@/components/table/GeneralTable'
 import WalletStatus from '@/components/walletStatus/WalletStatus'
 import {
@@ -9,6 +11,11 @@ import {
 import { parseFormattedNumber, parseNativeTokenToHuman } from '@/utils'
 import { useBalance, useInkathon } from '@poppyseed/lastic-sdk'
 import { useEffect, useState } from 'react'
+
+interface ModalDataType {
+  coreInfo: AllowedRenewalCoreInfoUnf
+  assignmentInfo: AllowedRenewalAssignmentInfo
+}
 
 // Custom hook for querying and transforming workplan data
 const useAllowedRenewalsQuery = () => {
@@ -41,6 +48,9 @@ const useAllowedRenewalsQuery = () => {
 }
 
 const RenewalsData = () => {
+  const [isRenewModalOpen, setIsRenewModalOpen] = useState(false)
+  const [modalData, setModalData] = useState<ModalDataType | null>(null)
+
   const { activeAccount, activeChain } = useInkathon()
   const allowedRenewals = useAllowedRenewalsQuery()
   const [task, setTask] = useState<number | null>(null)
@@ -74,88 +84,113 @@ const RenewalsData = () => {
   }
 
   return (
-    <Border className="h-full flex flex-row justify-center items-center">
-      <div className="h-full w-full flex flex-col justify-start items-start p-10">
-        <h1 className="text-xl font-bold uppercase mb-5">Cores for renewal</h1>
-        <div className="flex flex-row items-center gap-3 mb-5">
-          <label htmlFor="task">Task:</label>
-          <input
-            id="task"
-            type="number"
-            placeholder="Task Number"
-            value={task || ''}
-            onChange={(e) => setTask(parseFloat(e.target.value) || null)}
-            className="ml-2 p-2 border rounded"
-          />
-          <label htmlFor="begin">Begin:</label>
-          <input
-            id="begin"
-            type="number"
-            placeholder="Begin Number"
-            value={begin || ''}
-            onChange={(e) => setBegin(parseFloat(e.target.value) || null)}
-            className="p-2 border rounded"
-          />
-          <label htmlFor="core">Core:</label>
-          <input
-            id="core"
-            type="number"
-            placeholder="Core Number"
-            value={core || ''}
-            onChange={(e) => setCore(parseFloat(e.target.value) || null)}
-            className="p-2 border rounded"
-          />
-        </div>
-        {filteredData && filteredData.length > 0 ? (
-          <>
-            <div className="w-full overflow-x-auto">
-              <GeneralTable
-                tableData={filteredData.map(({ coreInfo, assignmentInfo }) => ({
-                  data: [
-                    assignmentInfo.completion?.Complete[0]?.assignment.Task || 'N/A',
-                    coreInfo[0].when,
-                    coreInfo[0].core,
-                    assignmentInfo.completion?.Complete[0]?.mask || 'N/A',
-                    `${parseNativeTokenToHuman({ paid: assignmentInfo.price?.toString(), decimals: 12, reduceDecimals: 6 })} ${tokenSymbol}`,
-                    //<PrimaryButton title='Renew' />,
-                  ],
-                }))}
-                tableHeader={[
-                  { title: 'Task' },
-                  { title: 'Begin' },
-                  { title: 'Core' },
-                  { title: 'Mask' },
-                  { title: 'Price' },
-                  //{ title: 'Click to Renew' },
-                ]}
-                colClass="grid-cols-5"
-              />
-            </div>
+    <>
+      <Border className="h-full flex flex-row justify-center items-center">
+        <div className="h-full w-full flex flex-col justify-start items-start p-10">
+          <h1 className="text-xl font-bold uppercase mb-5">Cores for renewal</h1>
+          <div className="flex flex-row items-center gap-3 mb-5">
+            <label htmlFor="task">Task:</label>
+            <input
+              id="task"
+              type="number"
+              placeholder="Task Number"
+              value={task || ''}
+              onChange={(e) => setTask(parseFloat(e.target.value) || null)}
+              className="ml-2 p-2 border rounded"
+            />
+            <label htmlFor="begin">Begin:</label>
+            <input
+              id="begin"
+              type="number"
+              placeholder="Begin Number"
+              value={begin || ''}
+              onChange={(e) => setBegin(parseFloat(e.target.value) || null)}
+              className="p-2 border rounded"
+            />
+            <label htmlFor="core">Core:</label>
+            <input
+              id="core"
+              type="number"
+              placeholder="Core Number"
+              value={core || ''}
+              onChange={(e) => setCore(parseFloat(e.target.value) || null)}
+              className="p-2 border rounded"
+            />
+          </div>
+          {filteredData && filteredData.length > 0 ? (
+            <>
+              <div className="w-full overflow-x-auto">
+                <GeneralTable
+                  tableData={filteredData.map(({ coreInfo, assignmentInfo }) => ({
+                    data: [
+                      assignmentInfo.completion?.Complete[0]?.assignment.Task || 'N/A',
+                      coreInfo[0].when,
+                      coreInfo[0].core,
+                      assignmentInfo.completion?.Complete[0]?.mask || 'N/A',
+                      `${parseNativeTokenToHuman({ paid: assignmentInfo.price?.toString(), decimals: 12, reduceDecimals: 4 })} ${tokenSymbol}`,
+                      <SecondaryButton
+                        title="Renew"
+                        onClick={() => {
+                          setModalData({ coreInfo: coreInfo[0], assignmentInfo })
+                          setIsRenewModalOpen(true)
+                        }}
+                        key="data"
+                      />,
+                    ],
+                  }))}
+                  tableHeader={[
+                    { title: 'Para ID' },
+                    { title: 'Begin' },
+                    { title: 'Core' },
+                    { title: 'Mask' },
+                    { title: 'Price' },
+                    { title: 'Renew' },
+                  ]}
+                  colClass="grid-cols-6"
+                />
+              </div>
 
-            {/* Pagination buttons */}
-            <div className="flex w-full items-center justify-between space-x-2 mt-4 px-5">
-              <button
-                onClick={handlePrevPage}
-                disabled={currentPage === 1}
-                className={`px-4 py-2 rounded-2xl text-black dark:text-gray-1 border border-gray-21 font-semibold ${currentPage === 1 ? 'bg-gray-2 dark:bg-gray-20 dark:text-gray-14 text-gray-14 cursor-not-allowed' : ' hover:bg-green-6'}`}
-              >
-                Previous
-              </button>
-              <p className="text-black dark:text-gray-1 font-semibold">{currentPage}</p>
-              <button
-                onClick={handleNextPage}
-                disabled={filteredData.length < itemsPerPage || filteredData.length === 0}
-                className={`px-4 py-2   border border-gray-21 text-black dark:text-gray-1 font-semibold rounded-2xl ${filteredData.length < itemsPerPage ? 'bg-gray-2 dark:bg-gray-20 dark:text-gray-14 text-gray-14 cursor-not-allowed' : ' hover:bg-green-6'}`}
-              >
-                Next
-              </button>
-            </div>
-          </>
-        ) : (
-          <p className="p-10">No data available.</p>
-        )}
-      </div>
-    </Border>
+              {/* Pagination buttons */}
+              <div className="flex w-full items-center justify-between space-x-2 mt-4 px-5">
+                <button
+                  onClick={handlePrevPage}
+                  disabled={currentPage === 1}
+                  className={`px-4 py-2 rounded-2xl text-black dark:text-gray-1 border border-gray-21 font-semibold ${currentPage === 1 ? 'bg-gray-2 dark:bg-gray-20 dark:text-gray-14 text-gray-14 cursor-not-allowed' : ' hover:bg-green-6'}`}
+                >
+                  Previous
+                </button>
+                <p className="text-black dark:text-gray-1 font-semibold">{currentPage}</p>
+                <button
+                  onClick={handleNextPage}
+                  disabled={filteredData.length < itemsPerPage || filteredData.length === 0}
+                  className={`px-4 py-2   border border-gray-21 text-black dark:text-gray-1 font-semibold rounded-2xl ${filteredData.length < itemsPerPage ? 'bg-gray-2 dark:bg-gray-20 dark:text-gray-14 text-gray-14 cursor-not-allowed' : ' hover:bg-green-6'}`}
+                >
+                  Next
+                </button>
+              </div>
+            </>
+          ) : (
+            <p className="p-10">No data available.</p>
+          )}
+        </div>
+      </Border>
+
+      {/* Renew modal */}
+      {modalData && (
+        <RenewModal
+          isOpen={isRenewModalOpen}
+          onClose={() => setIsRenewModalOpen(false)}
+          price={modalData.assignmentInfo.price}
+          tokenSymbol={tokenSymbol}
+          task={modalData.assignmentInfo.completion?.Complete[0]?.assignment.Task}
+          regionId={{
+            begin: modalData.coreInfo.when,
+            core: modalData.coreInfo.core,
+            mask: modalData.assignmentInfo.completion?.Complete[0]?.mask,
+          }}
+        />
+      )}
+    </>
   )
 }
 
