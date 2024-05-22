8269610fed0e5bf04aaff87603547f3a487b3e5c
Aurora Poppyseed <aurora.makovac@gmail.com>
2024-05-22 05:45:58 +0200

    adding number field

---
+++ src/components/inputField/NumberField.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from 'react'
+
+interface NumberFieldProps {
+  label: string
+  value: number | string
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  className?: string
+  inputClassName?: string
+}
+
+const NumberField: FC<NumberFieldProps> = ({
+  label,
+  value,
+  onChange,
+  className,
+  inputClassName,
+}) => {
+  return (
+    <div className={`flex flex-row justify-start items-center w-full space-x-4 ${className}`}>
+      <label className="text-md font-semibold mb-2">{label}</label>
+      <input
+        className={`text-lg border w-full bg-gray-100 dark:bg-gray-800 text-black dark:text-white rounded-md p-2 mb-4 focus:ring-blue-500 focus:border-blue-500 ${inputClassName}`}
+        type="number"
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  )
+}
+
+export default NumberField
