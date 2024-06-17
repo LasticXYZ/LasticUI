069495afb30e22751787e9f2f0c09d2c77e11784
Aurora Poppyseed <aurora.makovac@gmail.com>
2024-06-17 16:28:45 +0200

    adding feedback to component

--- src/components/table/GeneralTable.tsx
+++ src/components/table/GeneralTable.tsx
@@ -1,9 +1,9 @@
 import { toShortAddress } from '@/utils'
 import { joinClassNames } from '@/utils/helperFunc' // This is a custom function to join class names
+import { CheckIcon, ClipboardIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
-import { ClipboardIcon } from '@heroicons/react/24/outline'
 
 type GeneralTableProps = {
   tableData: Array<{ href?: string; data: Array<string | JSX.Element | undefined | null> }>
@@ -16,6 +16,15 @@ const GeneralTable: FC<GeneralTableProps> = ({
   tableHeader,
   colClass = 'grid-cols-4',
 }) => {
+  const [copiedIndex, setCopiedIndex] = useState<number | null>(null)
+
+  const handleCopy = (index: number) => {
+    setCopiedIndex(index)
+    setTimeout(() => {
+      setCopiedIndex(null)
+    }, 2000) // Clear the message after 2 seconds
+  }
+
   return (
     <div className="flex flex-col">
       <div className="relative overflow-x-auto py-5 px-5">
@@ -63,14 +72,26 @@ const GeneralTable: FC<GeneralTableProps> = ({
                           <div className="flex justify-start items-center p-4" key={innerIndex}>
                             {innerIndex === 2 ? (
                               <div className="cursor-pointer">
-                                <CopyToClipboard text={item2 as string}>
-                                  <span>{toShortAddress(item2 as string, 5)}
-                                          <ClipboardIcon
-                                              width={16}
-                                              height={16}
-                                            />
-                                        </span>
+                                <CopyToClipboard
+                                  text={item2 as string}
+                                  onCopy={() => handleCopy(index)}
+                                >
+                                  <span className="flex justify-center items-center gap-2">
+                                    {toShortAddress(item2 as string, 5)}
+                                    {copiedIndex === index ? (
+                                      <CheckIcon
+                                        width={16}
+                                        height={16}
+                                        className="text-green-500"
+                                      />
+                                    ) : (
+                                      <ClipboardIcon width={16} height={16} />
+                                    )}
+                                  </span>
                                 </CopyToClipboard>
+                                {copiedIndex === index && (
+                                  <span className="text-green-500 text-xs ml-2">Copied!</span>
+                                )}
                               </div>
                             ) : (
                               <>{item2}</>
